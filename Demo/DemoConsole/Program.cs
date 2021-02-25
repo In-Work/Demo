aa134140ee60e1593d6a7b357ef5d900dbead537
Zhenia <mr.blinger@gmail.com>
2021-02-25 23:59:16 +0300

    Добавление консольного приложения

---
+++ Demo/DemoConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int x = 3;
+         int y = 3;
+
+         Console.WriteLine(y + x);
+      }
+   }
+}
